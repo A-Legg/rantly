ff747101ef5ffefafe855c75161063c38a4fcfca
Adam Legg <adam.legg@gmail.com>
2014-10-23 13:38:51 -0600

    redid front end

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -7,12 +7,12 @@ class FollowingRelationshipsController < ApplicationController
 
   def create
     current_user.follow(user)
-    redirect_to dashboard_path(current_user)
+    redirect_to :back
   end
 
   def destroy
     current_user.unfollow(user)
-    redirect_to dashboard_path(current_user)
+    redirect_to :back
   end
 
   private
