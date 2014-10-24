041b7ef8cb0a3326a9496e9f00f1bbc7fb42978b
Adam Legg <adam.legg@gmail.com>
2014-10-24 11:38:14 -0600

    search by username functional

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
 
   def index
     @rant = Rant.new
-    @users = User.where('last_name LIKE ? OR first_name LIKE ?', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%')
+    @users = User.where('last_name LIKE ? OR first_name LIKE ? OR username lIKE ?', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%')
   end
 
 
