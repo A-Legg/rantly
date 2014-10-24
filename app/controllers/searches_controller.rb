36a1626ad4836494f38476d73ddb6b3bb97e745e
Adam Legg <adam.legg@gmail.com>
2014-10-24 11:48:15 -0600

    search rant title and body functional

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@ class SearchesController < ApplicationController
 
   def index
     @rant = Rant.new
+    @rants = Rant.where('description LIKE ? OR rant LIKE ?', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%')
     @users = User.where('last_name LIKE ? OR first_name LIKE ? OR username lIKE ?', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%')
   end
 
