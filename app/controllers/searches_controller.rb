e11efe832558272a068a7a41e33725ad6d4c718b
Adam Legg <adam.legg@gmail.com>
2014-10-28 14:23:50 -0600

    removed avatar from user show

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,5 +6,4 @@ class SearchesController < ApplicationController
     @users = User.where('last_name LIKE ? OR first_name LIKE ? OR username lIKE ?', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%')
   end
 
-
 end
