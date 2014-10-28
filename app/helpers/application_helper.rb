1782e3c80b38a4ec367c80f31396b8c6b062695f
Adam Legg <adam.legg@gmail.com>
2014-10-28 14:36:53 -0600

    cleaned up seach index and removed repeat code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,4 @@
 module ApplicationHelper
 
-  def follow_link(user)
-    if current_user.following?(user)
-      link_to "Unfollow", user_following_relationship_path(:id => current_user.id, :user_id => user.id), {method: :delete}
-    else
-      link_to "Follow", user_following_relationships_path(:user_id => user.id, :id => current_user.id), {method: :post}
-    end
-  end
-
-  def favorite_link(rant)
-    favorite = Favorite.where(rant_id: rant.id, user_id: current_user.id).first
-    if favorite
-      link_to "Unfavorite", user_favorite_path(:rant_id => rant.id, :user_id => current_user.id), {method: :delete}
-    else
-      link_to "Favorite", user_favorites_path(rant_id: rant.id, user_id: current_user.id), {method: :post}
-    end
-  end
+  
 end
