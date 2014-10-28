efbf1a6d743ae4756341b6551f432d7d75739de4
Adam Legg <adam.legg@gmail.com>
2014-10-28 15:36:12 -0600

    fixed search to not display all content

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,13 @@
 class SearchesController < ApplicationController
 
   def index
+    params[:search] ||= "Search Here"
     @rant = Rant.new
     @rants = Rant.where('description LIKE ? OR rant LIKE ?', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%')
     @users = User.where('last_name LIKE ? OR first_name LIKE ? OR username lIKE ?', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%', '%' + (params[:search].to_s) +'%')
   end
+
+
+
 
 end
