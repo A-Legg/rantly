50f458b8eb29438f162a87b0ad78b0ad9f17512f
Adam Legg <adam.legg@gmail.com>
2014-11-10 17:21:37 -0700

    welcome email functional

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,11 +32,13 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
-
+  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+    config.action_mailer.perform_deliveries = true
+  end
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
