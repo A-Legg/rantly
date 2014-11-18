06ad97dc5d09ad4756437f8c0218db625da6a4af
Adam Legg <adam.legg@gmail.com>
2014-11-18 15:50:55 -0700

    revert back

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,10 +40,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 3000 }
-
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'http://rantly-adam.herokuapp.com/' }
+  
 
 
   # config.action_mailer.delivery_method = :smtp
