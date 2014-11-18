3c3880c4798a285eeceff18b2a5522a34125a52f
Adam Legg <adam.legg@gmail.com>
2014-11-17 17:35:22 -0700

    admin rants odered by created_at

--- app/controllers/admin/rants_controller.rb
+++ app/controllers/admin/rants_controller.rb
@@ -2,8 +2,19 @@ class Admin::RantsController < AdminController
   before_filter :admin
 
   def index
+    if params[:start_date].present? && params[:end_date].present?
     @rants = Rant.where(created_at: params[:start_date]..params[:end_date])
+    elsif !params[:start_date].present? && params[:end_date].present?
+      @rants = Rant.where(["created_at < ?", params[:end_date]])
+    elsif params[:start_date].present? && !params[:end_date].present?
+      @rants = Rant.where(["created_at > ?", params[:start_date]])
+    else
+      @rants = Rant.all.order("created_at DESC")
+    end
   end
+
+
+
 
   def show
     @rant = Rant.find(params[:id])
--- app/controllers/admin/spams_controller.rb
+++ app/controllers/admin/spams_controller.rb
@@ -2,9 +2,21 @@ class Admin::SpamsController < AdminController
   before_filter :admin
 
   def index
-    @spams = Spam.where(created_at: params[:start_date]..params[:end_date])
+
+    if params[:start_date].present? && params[:end_date].present?
+      @spams = Spam.where(created_at: params[:start_date]..params[:end_date])
+    elsif !params[:start_date].present? && params[:end_date].present?
+      @spams = Spam.where(["created_at < ?", params[:end_date]])
+    elsif params[:start_date].present? && !params[:end_date].present?
+      @spams = Spam.where(["created_at > ?", params[:start_date]])
+    else
+      @spams = Spam.all.order("created_at DESC")
+    end
 
   end
+
+
+
 
   def destroy
     @spam = Spam.find_by(params[:spam_id])
